55e595468b1511f2dac26bf16ef8070a410a1c25
Dan Geiger <gaiger6@gmail.com>
2021-12-20 08:02:17 +0200

    progress, branch shape functioality

--- src/js/model.js
+++ src/js/model.js
@@ -1,18 +1,11 @@
-  //----------------------model-------------------
-  let data = {
-    currentPoint:[],
-    continuousPoint: [],
-  }
-  
-  class Shape{
-    constructor(name, type){
-      this.name = name;
-      this.type = type;
-      this.clickPoints = [];
-      this.editMode = true;
+//----------------------model-------------------
+
+class Shape {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+        this.clickPoints = [];
+        this.editMode = true;
     }
-    boundingBox(){
-      
-    }
-    
-  }
+    boundingBox() {}
+}
